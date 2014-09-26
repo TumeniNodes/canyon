1ab0a99e3c47763ca7ccbb1bb6d18ab27a48c429
paramat <mat.gregory@virginmedia.com>
2014-09-26 04:05:33 +0100

    Voxelmanip version

--- init.lua
+++ init.lua
@@ -1,131 +1,160 @@
--- canyon 0.3.1 by paramat.
--- For latest stable Minetest and back to 0.4.3.
--- Depends default.
--- License WTFPL.
+-- canyon 0.4.0 by paramat
+-- For latest stable Minetest and back to 0.4.8
+-- Depends default
+-- License WTFPL
 
 -- Parameters
 
-local ONGEN = true -- (true / false) Enable / disable canyon.
-local WAFAV = 0.4 -- 0.4 -- Water factor average.
-local WAFAMP = 0.7 -- 0.7 -- Water factor amplitude.
---Water factor is proportion of water surface level to removed stone surface level.
-local MINDEP = 11 -- 11 -- (0-30) Minimum river depth.
-local MAXDEP = 30 -- 30 -- (0-30) Maximum river depth.
-local PROG = true
+-- Water factor is proportion of water surface level to removed stone surface level
+local WAFAV = 0.4 -- Water factor average
+local WAFAMP = 0.7 -- Water factor amplitude
+local MINDEP = 11 -- (0-30) Minimum river depth
+local MAXDEP = 30 -- (0-30) Maximum river depth
+local TRIVERL = -0.06
+local TRIVERH = 0.06
 
-local SEEDDIFF1 = 5192098 -- Perlin1 for river pattern.
-local OCTAVES1 = 5 -- 5
-local PERSISTENCE1 = 0.6 -- 0.6
-local SCALE1 = 384 -- 384
-local NOISEL = -0.06 -- -0.06 -- NOISEL and NOISEH control canyon width.
-local NOISEH = 0.06 -- 0.06
+-- 2D noise for river channel
 
-local SEEDDIFF2 = 924 -- Perlin2 for depth variation.
-local OCTAVES2 = 4 -- 4
-local PERSISTENCE2 = 0.5 -- 0.5
-local SCALE2 = 192 -- 192
+local np_river = {
+	offset = 0,
+	scale = 1,
+	spread = {x=384, y=384, z=384},
+	seed = 5192098,
+	octaves = 5,
+	persist = 0.6
+}
 
-local SEEDDIFF3 = 13050 -- Perlin3 for water factor variation.
-local OCTAVES3 = 2 -- 2
-local PERSISTENCE3 = 0.4 -- 0.4
-local SCALE3 = 512 -- 512
+-- 2D noise for depth variation
+
+local np_depth = {
+	offset = 0,
+	scale = 1,
+	spread = {x=192, y=192, z=192},
+	seed = 924,
+	octaves = 4,
+	persist = 0.5
+}
+
+-- 2D noise for water factor
+
+local np_factor = {
+	offset = 0,
+	scale = 1,
+	spread = {x=512, y=512, z=512},
+	seed = 13050,
+	octaves = 2,
+	persist = 0.4
+}
 
 -- Stuff
 
-canyon = {}
-
 local depran = MAXDEP - MINDEP
-local noiran = NOISEH - NOISEL
+local noiran = TRIVERH - TRIVERL
 
 -- On generated function
 
-if ONGEN then
-	minetest.register_on_generated(function(minp, maxp, seed)
-		if minp.y == -32 then
-			local env = minetest.env
-			local perlin1 = env:get_perlin(SEEDDIFF1, OCTAVES1, PERSISTENCE1, SCALE1)
-			local perlin2 = env:get_perlin(SEEDDIFF2, OCTAVES2, PERSISTENCE2, SCALE2)
-			local perlin3 = env:get_perlin(SEEDDIFF3, OCTAVES3, PERSISTENCE3, SCALE3)
-			local x1 = maxp.x
-			local z1 = maxp.z
-			local x0 = minp.x
-			local z0 = minp.z
-			for x = x0, x1 do
-				if PROG then
-					print ("[canyon] "..(x - x0 + 1).." ("..minp.x.." "..minp.y.." "..minp.z..")")
+minetest.register_on_generated(function(minp, maxp, seed)
+	if not (minp.y < 1 and maxp.y > 1) then -- if not surface chunk
+		return
+	end
+		local t0 = os.clock()
+	local x0 = minp.x
+	local x1 = maxp.x
+	local y0 = minp.y
+	local y1 = maxp.y
+	local z0 = minp.z
+	local z1 = maxp.z
+	print ("[canyon] chunk minp ("..x0.." "..y0.." "..z0..")")
+
+	local vm, emin, emax = minetest.get_mapgen_object("voxelmanip")
+	local area = VoxelArea:new{MinEdge=emin, MaxEdge=emax}
+	local data = vm:get_data()
+
+	local c_air = minetest.get_content_id("air")
+	local c_water = minetest.get_content_id("default:water_source")
+	local c_waterflow = minetest.get_content_id("default:water_flowing")
+	local c_stone = minetest.get_content_id("default:stone")
+	local c_destone = minetest.get_content_id("default:desert_stone")
+	local c_sand = minetest.get_content_id("default:sand")
+
+	local sidelen = x1 - x0 + 1
+	local emerlen = sidelen + 32
+	local chulens = {x=sidelen, y=sidelen, z=sidelen}
+	local minposxz = {x=x0, y=z0}
+
+	local nvals_river = minetest.get_perlin_map(np_river, chulens):get2dMap_flat(minposxz)
+	local nvals_depth = minetest.get_perlin_map(np_depth, chulens):get2dMap_flat(minposxz)
+	local nvals_factor = minetest.get_perlin_map(np_factor, chulens):get2dMap_flat(minposxz)
+		local nixz = 1
+	for z = z0, z1 do
+	for x = x0, x1 do
+		local n_river = nvals_river[nixz]
+		if n_river > TRIVERL and n_river < TRIVERH then -- if column in river channel
+			local n_depth = nvals_depth[nixz]
+			local norm1 = (n_river - TRIVERL) * (TRIVERH - n_river) / noiran ^ 2 * 4
+			local norm2 = (n_depth + 1.875) / 3.75
+			-- find surface y and stone surface y
+			local ysurf = 1
+			local ystone = 1
+			local vi = area:index(x, y1, z)
+			for y = y1, 2, -1 do
+				local nodid = data[vi]
+				if ysurf == 1 and nodid ~= c_air then
+					ysurf = y
 				end
-				for z = z0, z1 do -- For each column do
-					local noise1 = perlin1:get2d({x=x,y=z})
-					if noise1 > NOISEL and noise1 < NOISEH then -- If column is in canyon then
-						local noise2 = perlin2:get2d({x=x,y=z})
-						local norm1 = (noise1 - NOISEL) * (NOISEH - noise1) / noiran ^ 2 * 4
-						local norm2 = (noise2 + 1.875) / 3.75
-						-- Find surface y
-						local surfacey = 1
-						for y = 47, 2, -1 do
-							local nodename = env:get_node({x=x,y=y,z=z}).name
-							if nodename ~= "air" then
-								surfacey = y
-								break
-							end
-						end
-						-- Find stone y
-						local stoney = 1
-						for y = 47, 2, -1 do
-							local nodename = env:get_node({x=x,y=y,z=z}).name
-							if nodename == "default:stone"
-							or nodename == "default:desert_stone"
-							or nodename == "landup:upstone" then
-								stoney = y
-								break
-							end
-						end
-						-- Calculate water surface rise and riverbed sand bottom y
-						local noise3 = perlin3:get2d({x=x,y=z})
-						local watfac = WAFAV + noise3 * WAFAMP
-						if watfac < 0 then
-							watfac = 0
-						end
-						if watfac > 0.9 then
-							watfac = 0.9
-						end
-						local watris = math.floor((stoney - 1) * watfac)
-						local exboty = surfacey - math.floor(norm1 * (surfacey - watris + 2 + MINDEP + norm2 * depran))
-						-- Find seabed y or airgap y
-						local seabedy = 47
-						for y = exboty, 47 do
-							local nodename = env:get_node({x=x,y=y,z=z}).name
-								if nodename == "default:water_source"
-								or nodename == "default:water_flowing"
-								or nodename == "air" then
-								seabedy = y - 1
-								break
-							end
-						end
-						-- Excavate canyon, add sand if below seabed or airgap, add water up to varying height, dig surface
-						for y = exboty, surfacey do
-							if y <= exboty + 2 and y <= seabedy and y <= watris + 2 then
-								env:add_node({x=x,y=y,z=z}, {name="default:sand"})
-							elseif y < watris + 1 then
-								env:add_node({x=x,y=y,z=z}, {name="default:water_source"})
-							elseif y == watris + 1 then
-								env:add_node({x=x,y=y,z=z}, {name="default:water_source"})
-								env:dig_node({x=x,y=y+1,z=z})
-							elseif y == surfacey then
-								env:dig_node({x=x,y=y,z=z})
-							else
-								env:remove_node({x=x,y=y,z=z})
-							end
-						end
-						-- Remove moss created by digging snow
-						local nodename = env:get_node({x=x,y=surfacey,z=z}).name
-						if nodename == "snow:moss" then
-							env:dig_node({x=x,y=surfacey,z=z})
-						end
-					end
+				if nodid == c_stone
+				or nodid == c_destone then
+					ystone = y
+					break
 				end
+				vi = vi - emerlen
+			end
+			-- Calculate water surface rise and riverbed sand bottom y
+			local n_factor = nvals_factor[nixz]
+			local watfac = WAFAV + n_factor * WAFAMP
+			watfac = math.min(math.max(watfac, 0), 0.9)
+			local watris = math.floor((ystone - 1) * watfac)
+			local yexbot = ysurf - math.floor(norm1 *
+			(ysurf - watris + 2 + MINDEP + norm2 * depran))
+			-- Find seabed y or airgap y
+			local yseabed = y1
+			local vi = area:index(x, yexbot, z)
+			for y = yexbot, y1 do
+				local nodid = data[vi]
+				if nodid == c_water
+				or nodid == c_waterflow
+				or nodid == c_air then
+					yseabed = y - 1
+					break
+				end
+				vi = vi + emerlen
+			end
+			-- Excavate canyon, add sand if below seabed or airgap
+			-- add water up to varying height, dig surface
+			local vi = area:index(x, yexbot, z)
+			for y = yexbot, ysurf do
+				if y <= yexbot + 2 and y <= yseabed
+				and y <= watris + 2 then
+					data[vi] = c_sand
+				elseif y <= watris + 1 then
+					data[vi] = c_water
+				else
+					data[vi] = c_air
+				end
+				vi = vi + emerlen
 			end
 		end
-	end)
-end
+		nixz = nixz + 1
+	end
+	end
+
+	vm:set_data(data)
+	vm:set_lighting({day=0, night=0})
+	vm:calc_lighting()
+	vm:write_to_map(data)
+	vm:update_liquids()
+
+	local chugent = math.ceil((os.clock() - t0) * 1000)
+	print ("[canyon] "..chugent.." ms")
+end)
 
